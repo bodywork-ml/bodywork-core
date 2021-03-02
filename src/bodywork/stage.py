c10f7d6274a93932e23dc46a440873c499025153
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-03-02 12:15:41 +0000

    Get service stage INGRESS config from config.ini

--- src/bodywork/stage.py
+++ src/bodywork/stage.py
@@ -170,9 +170,18 @@ class ServiceStage(Stage):
         except (KeyError, ValueError) as e:
             raise BodyworkStageConfigError('PORT', 'service', name) from e
 
+        try:
+            if config['service']['INGRESS'] not in ('True', 'False'):
+                raise ValueError('expected boolean value')
+            else:
+                create_ingress = config['service']['INGRESS']
+        except (KeyError, ValueError) as e:
+            raise BodyworkStageConfigError('INGRESS', 'service', name) from e
+
         self.replicas = replicas
         self.max_startup_time = max_startup_time
         self.port = port
+        self.create_ingress = create_ingress
         super().__init__(name, config, path_to_stage_dir)
 
 
