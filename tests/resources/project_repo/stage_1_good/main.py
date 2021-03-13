78fc49ed906055397d42c4118c0e8933a1dc4a95
Marios <mariosi@hotmail.co.uk>
2021-03-13 06:14:01 +0000

    Updated test stage output paths

--- tests/resources/project_repo/stage_1_good/main.py
+++ tests/resources/project_repo/stage_1_good/main.py
@@ -7,7 +7,7 @@ import numpy
 
 def main() -> None:
     count_to_ten = numpy.sum(numpy.ones(10))
-    with open('stage_1_test_file.txt', 'w') as f:
+    with open('../../bodywork_project_output/stage_1_test_file.txt', 'w') as f:
         f.write('Hello from stage 1\n')
         f.write(f'numpy.sum(numpy.ones(10))={count_to_ten}')
 
