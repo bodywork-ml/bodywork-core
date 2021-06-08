d2fb4fde698aea78d310fd9a86112a8d20fa645f
Marios <mariosi@hotmail.co.uk>
2021-06-08 21:02:08 +0800

    Change to use urllib

--- setup.py
+++ setup.py
@@ -15,60 +15,55 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from setuptools import find_packages, setup
-import urllib3
+import urllib.request
+import urllib.parse
 
 
 # get package version
-with open('VERSION') as version_file:
+with open("VERSION") as version_file:
     version = version_file.read().strip()
 
 # assemble requirements
-with open('requirements_pkg.txt') as f:
+with open("requirements_pkg.txt") as f:
     requirements_pkg = f.read().splitlines()
 
-with open('requirements_dev.txt') as f:
+with open("requirements_dev.txt") as f:
     requirements_dev = f.read().splitlines()
 
 # load the README file and use it as the long_description for PyPI
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     readme = f.read()
 
 setup(
-    name='bodywork',
-    description='Deploy machine learning to Kubernetes - MLOps accelerated.',
+    name="bodywork",
+    description="Deploy machine learning to Kubernetes - MLOps accelerated.",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     version=version,
-    license='AGPL 3.0',
-    author='Bodywork Machine Learning Ltd.',
-    author_email='info@bodyworkml.com',
-    url='https://www.bodyworkml.com',
+    license="AGPL 3.0",
+    author="Bodywork Machine Learning Ltd.",
+    author_email="info@bodyworkml.com",
+    url="https://www.bodyworkml.com",
     project_urls={
-        'Source': 'https://github.com/bodywork-ml/bodywork-core',
-        'Documentation': 'https://bodywork.readthedocs.io/en/latest/'
+        "Source": "https://github.com/bodywork-ml/bodywork-core",
+        "Documentation": "https://bodywork.readthedocs.io/en/latest/",
     },
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     include_package_data=True,
     python_requires=">=3.7.*",
     install_requires=requirements_pkg,
-    extras_require={
-        'dev': requirements_dev
-    },
+    extras_require={"dev": requirements_dev},
     zip_safe=True,
-    entry_points={
-        'console_scripts': [
-            'bodywork=bodywork.cli.cli:cli'
-        ]
-    },
+    entry_points={"console_scripts": ["bodywork=bodywork.cli.cli:cli"]},
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
+query_string = urllib.parse.urlencode({"type": "pip-install"})
+url = f"http://a9c1ef555dfcc4fa3897c9468920f8b7-032e5dc531a766e1.elb.eu-west-2.amazonaws.com/bodywork-ml/usage-tracking--server/workflow-execution-counter?{query_string}"  # noqa
 
-http = urllib3.PoolManager()
-resp = http.request('GET', "http://a9c1ef555dfcc4fa3897c9468920f8b7-032e5dc531a766e1.elb.eu-west-2.amazonaws.com/bodywork-ml/usage-tracking--server/workflow-execution-counter"  # noqa
-                    , fields={"type": "pip-install"})
+urllib.request.urlopen(url)
