3de3682515e3d3af15c63958c0bbb5e8d89296d0
Marios Ioannides <Mariosi@hotmail.co.uk>
2021-09-20 22:43:13 +0100

    Add Sleep to Beginning of test

--- tests/integration/test_k8s_with_cluster.py
+++ tests/integration/test_k8s_with_cluster.py
@@ -330,6 +330,7 @@ def test_workflow_with_ssh_github_connectivity(
 @mark.usefixtures("add_secrets")
 def test_deployment_of_remote_workflows(docker_image: str):
     job_name = "test-remote-workflows"
+    sleep(10)
     try:
         process_one = run(
             [
@@ -346,7 +347,7 @@ def test_deployment_of_remote_workflows(docker_image: str):
         assert process_one.returncode == 0
         assert f"workflow job={job_name} created" in process_one.stdout
 
-        sleep(20)
+        sleep(15)
 
         process_two = run(
             [
