d224f0d5ebfef5cfa4bedfdf55c2741f8f388254
Marios Ioannides <Mariosi@hotmail.co.uk>
2021-09-25 19:13:36 +0100

    Add comment

--- src/bodywork/workflow_execution.py
+++ src/bodywork/workflow_execution.py
@@ -190,6 +190,11 @@ def run_workflow(
 
 
 def _cleanup_redundant_services(git_commit_hash, namespace) -> None:
+    """Deletes services that are not part of this git commit.
+
+    :param git_commit_hash: Git commit hash of current deployment.
+    :param namespace: Namespace deployment is in.
+    """
     _log.info("Searching for services from previous deployment.")
     deployments = k8s.list_service_stage_deployments(namespace)
     for name, deployment in deployments.items():
