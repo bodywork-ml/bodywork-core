56c79ea22dd154bb7cc4f12c1f610a8b707bb3c2
alexioannides <alex.ioanndies@yahoo.co.uk>
2022-05-12 16:47:13 +0100

    Add new tests for new has_unschedulable_pods function

--- tests/unit_and_functional/test_k8s_utils.py
+++ tests/unit_and_functional/test_k8s_utils.py
@@ -17,9 +17,16 @@
 """
 Unit tests for k8s API helper functions.
 """
-from unittest.mock import Mock
+from unittest.mock import MagicMock, Mock, patch
 
-from bodywork.k8s.utils import api_exception_msg, make_valid_k8s_name
+import kubernetes
+from pytest import raises
+
+from bodywork.k8s.utils import (
+    api_exception_msg,
+    has_unscheduleable_pods,
+    make_valid_k8s_name,
+)
 
 
 def test_api_exception_msg_retreives_message_str():
@@ -30,6 +37,66 @@ def test_api_exception_msg_retreives_message_str():
 
     mock_api_exception.body = '{"bar": "foo"}'
     assert api_exception_msg(mock_api_exception) == ""
+
+
+@patch("kubernetes.client.CoreV1Api")
+def test_has_unscheduleable_pods_correctly_identifies_bad_pod_condition(
+    mock_k8s_core_api: MagicMock,
+):
+    mock_k8s_core_api().list_namespaced_pod.return_value = kubernetes.client.V1PodList(
+        items=[
+            kubernetes.client.V1Pod(
+                metadata=kubernetes.client.V1ObjectMeta(
+                    name="foo-123", namespace="bar"
+                ),
+                status=kubernetes.client.V1PodStatus(
+                    conditions=[
+                        kubernetes.client.V1PodCondition(
+                            reason="Unschedulable", status="False", type="PodScheduled"
+                        )
+                    ]
+                ),
+            )
+        ]
+    )
+    test_job = kubernetes.client.V1Job(
+        metadata=kubernetes.client.V1ObjectMeta(name="foo", namespace="bar")
+    )
+    assert has_unscheduleable_pods(test_job)
+
+
+@patch("kubernetes.client.CoreV1Api")
+def test_has_unscheduleable_pods_correctly_identifies_good_pod_condition(
+    mock_k8s_core_api: MagicMock,
+):
+    mock_k8s_core_api().list_namespaced_pod.return_value = kubernetes.client.V1PodList(
+        items=[
+            kubernetes.client.V1Pod(
+                metadata=kubernetes.client.V1ObjectMeta(
+                    name="foo-123", namespace="bar"
+                ),
+                status=kubernetes.client.V1PodStatus(conditions=[]),
+            )
+        ]
+    )
+    test_job = kubernetes.client.V1Job(
+        metadata=kubernetes.client.V1ObjectMeta(name="foo", namespace="bar")
+    )
+    assert not has_unscheduleable_pods(test_job)
+
+
+@patch("kubernetes.client.CoreV1Api")
+def test_has_unscheduleable_pods_raises_error_if_pods_not_found(
+    mock_k8s_core_api: MagicMock,
+):
+    mock_k8s_core_api().list_namespaced_pod.return_value = kubernetes.client.V1PodList(
+        items=[]
+    )
+    test_job = kubernetes.client.V1Job(
+        metadata=kubernetes.client.V1ObjectMeta(name="foo", namespace="bar")
+    )
+    with raises(RuntimeError, match="cannot find pods with names that start with"):
+        has_unscheduleable_pods(test_job)
 
 
 def test_make_valid_k8s_name_corrects_invalid_names():
