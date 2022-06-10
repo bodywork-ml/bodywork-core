43793970b8df86494d86768dabc46f8be02c9c4b
alexioannides <alex.ioanndies@yahoo.co.uk>
2022-06-10 20:10:15 +0100

    Handle batch job monitoring exceptions explicitly.

--- src/bodywork/workflow_execution.py
+++ src/bodywork/workflow_execution.py
@@ -46,6 +46,7 @@ from .constants import (
     SSH_SECRET_NAME,
 )
 from .exceptions import (
+    BodyworkJobFailure,
     BodyworkWorkflowExecutionError,
     BodyworkNamespaceError,
     BodyworkDockerImageError,
@@ -316,7 +317,7 @@ def _run_batch_stages(
             job_name = job_object.metadata.name
             _log.info(f"Completed k8s job for stage = {job_name}")
             _print_logs_to_stdout(namespace, job_name, False)
-    except Exception as e:
+    except (TimeoutError, BodyworkJobFailure) as e:
         _log.error("Some (or all) k8s jobs failed to complete successfully")
         for job_object in job_objects:
             job_name = job_object.metadata.name
